dc0ba832cb7545f089fd6944cf165f49e16d518c
mynameis7 <andrew.rather5@gmail.com>
2015-09-08 19:20:16 -0400

    gitignore is now working

--- compile.bat
+++
@@ -1,5 +0,0 @@
-cd src
-gui-compile.py
-cd ..\
-pyinstaller -F LogAnalyzerApp.spec > app_installation.log.txt
-pyinstaller -F LogAnalyzer.spec > cli_installation.log.txt
--- compile.ps1
+++
@@ -1,5 +0,0 @@
-cd src
-./gui-compile.py
-cd ..
-pyinstaller -F LogAnalyzerApp.spec
-pyinstaller -F LogAnalyzer.spec
